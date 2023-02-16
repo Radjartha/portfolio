c62b72dbcb811a8cbfc10938ec45313e6641a855
Radjartha <radjartha12@gmail.com>
2023-02-17 05:38:46 +0700

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html'],
+  content: ['index.html','info.html'],
+
   theme: {
     container: {
       center: true,
